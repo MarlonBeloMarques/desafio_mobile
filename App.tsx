d830c9374bcdbd597fbc037205d2c83963cedf89
Marlon Marques <marlon.belohd@gmail.com>
2021-08-18 11:30:50 -0400

    feat(themes): add theme variables

--- App.tsx
+++ App.tsx
@@ -10,11 +10,28 @@
 
 import React from 'react';
 import { SafeAreaView, StatusBar, Text } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { colors, spacings, radius, typography } from '~/themes';
+
+const theme = {
+  ...colors,
+  ...spacings,
+  ...radius,
+  ...typography,
+};
 
 const App = () => (
-  <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <StatusBar barStyle="light-content" />
-    <Text style={{ color: '#fff' }}>ByCoders Test</Text>
+  <SafeAreaView
+    style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" />
+      <Text style={{ color: '#fff' }}>ByCoders Test</Text>
+    </ThemeProvider>
   </SafeAreaView>
 );
 
