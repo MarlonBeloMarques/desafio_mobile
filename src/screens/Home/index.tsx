e09848bcbf3af77b93cfdf56525f565e84b18c26
Marlon Marques <marlon.belohd@gmail.com>
2021-08-19 08:31:49 -0400

    feat(screen/home): get device location

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Alert } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 import Home from './Home';
+import { hasLocationPermission } from './permission';
 
-const HomeContainer: React.FC = () => <Home />;
+const HomeContainer: React.FC = () => {
+  const getLocation = async () => {
+    const hasPermission = await hasLocationPermission();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    Geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+      },
+      (error) => {
+        Alert.alert(`Code ${error.code}`, error.message);
+        console.log(error);
+      },
+      {
+        accuracy: {
+          android: 'high',
+          ios: 'best',
+        },
+        enableHighAccuracy: true,
+        timeout: 15000,
+        maximumAge: 10000,
+        distanceFilter: 0,
+      },
+    );
+  };
+  useEffect(() => {
+    getLocation();
+  });
+
+  return <Home />;
+};
 
 export default HomeContainer;
